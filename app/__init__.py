6c54d10a5fac830c01c61744e150a10265180a27
joncoe <info@joncoe.com>
2023-10-05 18:10:42 -0400

    Lab 5 work

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import logging
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_appbuilder.security.mongoengine.manager import SecurityManager
 from flask_appbuilder import AppBuilder
 from flask_mongoengine import MongoEngine
@@ -8,11 +8,11 @@ from flask_mongoengine import MongoEngine
  Logging configuration
 """
 
-logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+logging.basicConfig(format="%(asctime)s:%(levelname)s:%(name)s:%(message)s")
 logging.getLogger().setLevel(logging.DEBUG)
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object("config")
 db = MongoEngine(app)
 appbuilder = AppBuilder(app, security_manager_class=SecurityManager)
 
@@ -28,7 +28,42 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
     cursor = dbapi_connection.cursor()
     cursor.execute("PRAGMA foreign_keys=ON")
     cursor.close()
-"""    
+"""
 
-from app import views
+from app import views, models
 
+
+@app.route("/list", methods=["GET"])
+def list_all_users():
+    data = list(models.User.objects)
+    return data
+
+
+@app.route("/insert", methods=["POST"])
+def create_user():
+    # retrieve data from html form
+    name = request.form.get("user_name")
+    email = request.form.get("user_email")
+    # create a user object with above data
+    new_user = models.User(name=name, email=email)
+    # persist user object to database
+    new_user.save()
+    return new_user.to_json()
+
+
+@app.route("/delete", methods=["POST"])
+def delete_user():
+    # retrieve data from html form
+    name = request.form.get("user_name")
+    # find all names that match
+    users = models.User.objects(name=name)
+    # get the first name from matching names
+    user = users.first()
+
+    if not user:
+        return jsonify({"error": "data not found"})
+    else:
+        user.delete()
+
+    status_message = user.name + " has been deleted"
+    return jsonify({"status": status_message})
